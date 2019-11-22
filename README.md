23532f6dd4d0b62cf706a40dc76011bccbcf0082
Lex Alexander <l.alexander10@gmail.com>
2019-11-22 02:13:14 -0800

    Added tsnode as dev dependecy

--- README.md
+++ README.md
@@ -7,42 +7,17 @@ A simple TypeScript module bundler, inspired by [minipack](https://github.com/ro
 - [x] Minimal dependency (only [the TypeScript Compiler API](https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API))
 - [x] Type check
 - [x] Bundle TypeScript modules (only ECMAScript Modules)
-- [x] Remove duplication for the same module
-- [x] Resolve circular dependency
-- [x] `node_modules` resolution
 
-## Install
-
-### Install globally with npm
-
-```shell
-npm install -g @cacheflow/Smallpack
-```
-
-Run with `Smallpack`.
 
 ### Manual install from source
 
 ```shell
 git clone https://github.com/cacheflow/Smallpack.git
-cd Smallpack
-yarn # or `npm i` should work too
+cd smallpack
+yarn  
+yarn run bundle
 ```
 
-Run `bin/Smallpack` in the project root.
-
-## How to use
-
-```shell
-Smallpack path/to/entry.ts
-```
-
-### Play with [examples](examples)
-
-```shell
-Smallpack examples/01-simple # stdout
-Smallpack examples/01-simple | node # run
-```
 
 ## How does it work?
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 "use strict";
 exports.__esModule = true;
-var fs_1 = require("fs");
 var path = require('path');
-var babylon = require('babylon');
+var fs_1 = require("fs");
+var uuidv1 = require('uuid/v1');
 var ts = require("typescript");
-var md5 = require('md5');
-var fileName = './sample/hello.ts';
+var Terser = require("terser");
+//include minifier
 var Queue = /** @class */ (function () {
     function Queue() {
         var _this = this;
@@ -19,13 +19,27 @@ var Queue = /** @class */ (function () {
     }
     return Queue;
 }());
+var removeApostrophes = function (data) {
+    return data.replace(/['"]/g, "");
+};
+var resovledDependencyPath = function (parentDir, depName) {
+    var depPath = removeApostrophes(depName);
+    return path.resolve(parentDir, depPath);
+};
+var resolvePath = function (data) {
+    if (data.startsWith('/') || data.startsWith('./')) {
+        return path.join(path.dirname(data), data);
+    }
+    return data;
+};
 var Module = /** @class */ (function () {
     function Module() {
         this.ast = {};
         this.fileName = '';
+        this.mapping = {};
         this.code = '';
         this.dependencies = new Map();
-        this.id = md5('');
+        this.id = uuidv1();
     }
     Module.prototype.addAst = function (ast) {
         if (Object.keys(this.ast).length >= 1) {
@@ -33,56 +47,88 @@ var Module = /** @class */ (function () {
         }
         this.ast = ast;
     };
+    Module.prototype.addCode = function (code) {
+        if (Object.keys(this.code).length >= 1) {
+            return;
+        }
+        var transpiledCode = ts.transpileModule(code, {
+            compilerOptions: {
+                module: ts.ModuleKind.CommonJS
+            }
+        });
+        this.code = transpiledCode.outputText || '';
+    };
     Module.prototype.addFileName = function (fileName) {
-        this.fileName = fileName;
+        this.fileName = resolvePath(fileName);
     };
     Module.prototype.addDependency = function (dependency) {
         if (!this.dependencies.has(dependency)) {
-            this.dependencies.set(dependency, dependency);
+            //lets create another module in here
+            var prependedDep = dependency.includes('.ts') ? dependency : dependency + ".ts";
+            this.dependencies.set(prependedDep, prependedDep);
         }
     };
     return Module;
 }());
-var module = new Module();
+var getDependencies = function (node, module) {
+    recursivelyGetDepdencies(node);
+    function recursivelyGetDepdencies(node) {
+        switch (node.kind) {
+            case ts.SyntaxKind.ImportDeclaration:
+                var importDecl = node;
+                var depName = importDecl.moduleSpecifier.getText();
+                var fileName_1 = importDecl.getSourceFile().fileName;
+                var parentDirName = path.dirname(fileName_1);
+                module.addDependency(resovledDependencyPath(parentDirName, depName));
+        }
+        ts.forEachChild(node, recursivelyGetDepdencies);
+    }
+    return module;
+};
+var arr = [];
+var walk = function (node) {
+    var module = new Module;
+    var sourceFile = node;
+    module.addAst(sourceFile);
+    module.addFileName(sourceFile.fileName);
+    module.addCode(sourceFile.text);
+    var moduleWithDependencies = getDependencies(sourceFile, module);
+    return moduleWithDependencies;
+};
 var createSourceFileAst = function (fileName) {
     var sourceFile = ts.createSourceFile(fileName, fs_1.readFileSync(fileName).toString(), ts.ScriptTarget.ES2015, 
     /*setParentNodes */ true);
     return sourceFile;
 };
-var removeApostrophes = function (data) {
-    return data.replace(/['"]/g, "");
+var buildModule = function (fileName) {
+    var ast = createSourceFileAst(fileName);
+    var module = walk(ast);
+    return module;
 };
-var walk = function (node) {
-    var sourceFile = node;
-    module.addAst(sourceFile);
-    switch (node.kind) {
-        case ts.SyntaxKind.ImportDeclaration:
-            var importDecl = node;
-            var sourceFile_1 = node;
-            var fName = importDecl.getSourceFile().fileName;
-            module.addFileName(fName);
-            module.addDependency(removeApostrophes(importDecl.moduleSpecifier.getText()));
-    }
-    ts.forEachChild(node, walk);
-};
-var bundle = function (module) {
+var getBaseName = function (fpath) { return "./" + path.basename(fpath, path.extname(fpath)); };
+var minify = function (code) { return Terser.minify(code); };
+var bundle = function (fileName) {
+    var module = buildModule(fileName);
     var queue = new Queue();
     queue.enqueue(module);
-    var _loop_1 = function () {
-        var data = queue.dequeue();
-        var queuedModule = data;
-        if (queuedModule) {
-            var fileNameDir_1 = path.join(process.cwd(), path.dirname(queuedModule.fileName));
-            queuedModule.dependencies.forEach(function (dep) {
-                var depAbsolutePath = path.join(fileNameDir_1, dep + ".ts");
-                var ast = createSourceFileAst(depAbsolutePath);
-                console.log('ast is ', ast);
-            });
-        }
-    };
+    module.dependencies.forEach(function (depPath) {
+        var child = buildModule(depPath);
+        module.mapping["" + getBaseName(depPath)] = child.id;
+        queue.enqueue(child);
+    });
+    var modules = '';
+    var initId = '';
     while (!queue.empty()) {
-        _loop_1();
+        var mod = queue.dequeue();
+        if (mod) {
+            if (!initId) {
+                initId = mod.id;
+            }
+            modules += "\"" + mod.id + "\": [\n        function (require, module, exports) {\n          " + mod.code + "\n        },\n        " + JSON.stringify(mod.mapping) + ",\n      ],";
+        }
     }
+    var result = "\n    (function(modules) {\n      function require(id) {\n        const [fn, mapping] = modules[id];\n        function localRequire(name) {\n          return require(mapping[name]);\n        }\n\n        const module = { exports : {} };\n\n        fn(localRequire, module, module.exports);\n        return module.exports;\n      }\n\n      require(\"" + initId + "\");\n    })({" + modules + "})\n  ";
+    return minify(result).code;
 };
-walk(createSourceFileAst(fileName));
-bundle(module);
+var fileName = './sample/hello.ts';
+console.log(bundle(fileName));
